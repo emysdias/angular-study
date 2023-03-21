ca433bd3ba490b52d175624f68aabf07c523dd78
emysdias <diassousa@yahoo.com.br>
2023-03-21 16:44:06 -0300

    add components

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myApp';
+  userName = 'Emily';
+  userData = {
+    email: 'diassousa@yahoo.com.br',
+    role: 'Developer',
+  };
 }
