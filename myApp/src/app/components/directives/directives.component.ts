e153ca456a71a265a4886737281b61449af442ca
emysdias <diassousa@yahoo.com.br>
2023-03-21 17:01:34 -0300

    add directive module

---
+++ myApp/src/app/components/directives/directives.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.css'],
+})
+export class DirectivesComponent {
+  size = 12;
+  classes = ['small-text', 'green-text'];
+}
